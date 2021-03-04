7269959aa0d7e21cb32ba9d9151600a9156af4d5
ChrisSHDev <kakeochee@naver.com>
2021-03-04 08:15:17 -0800

    Styling a hero section

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -13,7 +13,31 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 ?>
     <div class="home-banner">
-
+        <div class="container">
+            <h1>A Dining Experience</h1>
+        </div>
+    </div>
+    <div class="home-call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="call-to-action">
+                        <div class="image"></div>
+                        <div class="title">Dining Menu</div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="call-to-action second">
+                        <div class="image"></div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="call-to-action third">
+                        <div class="image"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 <?php
 get_footer();
