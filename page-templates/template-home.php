fa96318ec8629b3eb38ca58583bdecbeb0acc086
ChrisSHDev <kakeochee@naver.com>
2021-03-05 08:04:33 -0800

    finished welcome banner section

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -17,27 +17,62 @@ get_header();
             <h1>A Dining Experience</h1>
         </div>
     </div>
+
     <div class="home-call-to-action">
         <div class="container">
             <div class="row">
-                <div class="col-lg-4">
-                    <div class="call-to-action">
+                <div class="col-md-4">
+                    <a href="#" class="call-to-action">
                         <div class="image"></div>
                         <div class="title">Dining Menu</div>
-                    </div>
+                    </a>
                 </div>
-                <div class="col-lg-4">
-                    <div class="call-to-action second">
+                <div class="col-md-4">
+                    <a href="#" class="call-to-action second">
                         <div class="image"></div>
-                    </div>
+                        <div class="title">Lunch Menu</div>
+                    </a>
                 </div>
-                <div class="col-lg-4">
-                    <div class="call-to-action third">
+                <div class="col-md-4">
+                    <a href="#" class="call-to-action third">
                         <div class="image"></div>
-                    </div>
+                        <div class="title">Functions</div>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="home-welcome">
+        <div class="title-holder">
+            <div class="container">
+                <h2>
+                    <span> Welcome to </span>
+                    The Backbeach Eating House
+                </h2>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-1">
+                </div>
+                <div class="col-lg-8">
+                    <p class="tag-line">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </p>
+                    <p>
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+                    </p>
+                </div>
+                <div class="col-lg-3">
+                    <a href="#" class="button">
+                        Learn More <i class="fa fa-chevron-right"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php
 get_footer();
